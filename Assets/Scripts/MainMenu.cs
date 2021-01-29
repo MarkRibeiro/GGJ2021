2b9ca43c17668211e0361f102c1a3385fc76a4d5
Mark Ribeiro <40504056+MarkRibeiro@users.noreply.github.com>
2021-01-29 12:50:50 -0300

    Habilita botão jogar

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,8 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void QuitGame()
-    {
+    public string Level;
+    public void Play(){
+        SceneManager.LoadScene(Level);
+    }
+    public void QuitGame(){
+        Debug.Log("Saiu");
         Application.Quit();
     }
 }
