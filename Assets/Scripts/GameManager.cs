f7ecbb542859df642201e8b2449416c0635b7e9e
Mark Ribeiro <40504056+MarkRibeiro@users.noreply.github.com>
2021-01-29 18:21:57 -0300

    Ativa/desativa pause com "esc"

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool isPaused;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            pause();
+        }
+    }
+
+    void pause()
+    {
+        if (isPaused == false)
+        {
+            Time.timeScale = 0;
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseMenu.SetActive(false);
+        }
+        isPaused = !isPaused;
+        Debug.Log(Time.timeScale);
+    }
+}
