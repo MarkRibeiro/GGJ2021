a2d5934a0d60a74abea9362960fc41372cfff096
Mark Ribeiro <40504056+MarkRibeiro@users.noreply.github.com>
2021-01-30 01:56:12 -0300

    Coloca jogo em ingles

--- Assets/Scripts/ClickToHighlight.cs
+++ Assets/Scripts/ClickToHighlight.cs
@@ -15,29 +15,28 @@ public class ClickToHighlight : MonoBehaviour
     
     void Update()
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(ray.origin,ray.direction*100, Color.magenta, 0.02f);
-        if( Input.GetMouseButtonDown(0)){
-            RaycastHit hit;
-            
-            if(Physics.Raycast(ray,out hit, 1000.0f)){
-                GetComponent<Renderer>().sharedMaterial.SetVector("Vector3_C357230",hit.point);
-            }
-
-            if(treasure.treasure == false)
-            {
-                treasure.confirmScreen.SetActive(true);
-                Time.timeScale = 0;
-                if (treasure.areYouSure == true)
-                {
-                    TreasueManeger.localOfTreasure = hit.transform.position;
-                    treasure.treasure = true;
-                }
-            }
-            if(treasure == true)
-            {
-                TreasueManeger.localOfTreasure = hit.transform.position;
+        if(Time.timeScale == 1){    
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(ray.origin,ray.direction*100, Color.magenta, 0.02f);
+            if( Input.GetMouseButtonDown(0)){
+                RaycastHit hit;
                 
+                if(Physics.Raycast(ray,out hit, 1000.0f)){
+                    GetComponent<Renderer>().sharedMaterial.SetVector("Vector3_C357230",hit.point);
+                }
+
+                if(treasure.treasure == false){
+                    treasure.confirmScreen.SetActive(true);
+                    Time.timeScale = 0;
+                    if (treasure.areYouSure == true){
+                        TreasueManeger.localOfTreasure = hit.transform.position;
+                        treasure.treasure = true;
+                    }
+                }
+                if(treasure == true){
+                    TreasueManeger.localOfTreasure = hit.transform.position;
+                    
+                }
             }
         }
     }
