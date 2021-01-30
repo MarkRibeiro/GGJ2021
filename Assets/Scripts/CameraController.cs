9b51444ee410f070533ee883d6bbd4ac0c8cda17
NagibSuaid <nagibmoura@gmail.com>
2021-01-30 15:51:04 -0300

    camera livre e controlavel

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,92 +4,39 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float speed = 0.05f;
+    public float panSpeed = 10f;
     public float zoomSpeed = 10.0f;
-    public float rotationSpeed = 0.5f;
+    public float rotationSpeed = 10.0f;
 
-    public float maxHeight = 40f;
-    public float minHeight = 4f;
+    public Transform islandTransform;
+    public Transform cameraTransform;
 
-    Vector3 initialPosition;
-    Vector3 initialRotation;
-    Vector2 p1;
-    Vector2 p2;
+    private void Update() {
+        
+        Vector3 pos = cameraTransform.position;
+        pos += Input.GetAxis("Vertical") * Vector3.Normalize(new Vector3(cameraTransform.forward.x,0,cameraTransform.forward.z)) * panSpeed * Time.deltaTime;
+        pos += Input.GetAxis("Horizontal") * Vector3.Normalize(new Vector3(cameraTransform.right.x,0,cameraTransform.right.z)) * panSpeed * Time.deltaTime;
+        pos += Input.GetAxis("Mouse ScrollWheel") * cameraTransform.forward * zoomSpeed * Time.deltaTime;
+        cameraTransform.position = pos;
 
-    private Camera mainCamera;
-
-    private void OnValidate()
-    {
-        if(mainCamera == null)
-        {
-            mainCamera = Camera.main;
-        }
-    }
-    private void Start()
-    {
-        initialPosition = transform.position;
-        initialRotation.x = mainCamera.transform.eulerAngles.x;
-        initialRotation.y = mainCamera.transform.eulerAngles.y;
-        initialRotation.y = mainCamera.transform.eulerAngles.z;
-    }
-    void Update()
-    {
-        float horizontalSpeed = transform.position.y * speed * Input.GetAxis("Horizontal");
-        float verticalSpeed = transform.position.y * speed * Input.GetAxis("Vertical");
-        float scrollspeed = -zoomSpeed * Input.GetAxis("Mouse ScrollWheel");
-
-        if ((transform.position.y >= maxHeight) && (scrollspeed > 0))
-        {
-            scrollspeed = 0;
-        }
-        else if((transform.position.y <= minHeight) && (scrollspeed < 0))
-        {
-            scrollspeed = 0;
-        }
-
-        if((transform.position.y + scrollspeed) > maxHeight)
-        {
-            scrollspeed = maxHeight - transform.position.y;
-        }
-        if ((transform.position.y + scrollspeed) < minHeight)
-        {
-            scrollspeed = minHeight - transform.position.y;
-        }
-
-        Vector3 verticalMove = new Vector3(0, scrollspeed, 0);
-        Vector3 lateralMove = horizontalSpeed * transform.right;
-        Vector3 forwardMove = transform.forward;
-        forwardMove.y = 0;
-        forwardMove.Normalize();
-        forwardMove *= verticalSpeed;
-
-        Vector3 move = verticalMove + lateralMove + forwardMove;
-
-        transform.position += move;
         getCameraRotation();
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            transform.position = initialPosition;
-            mainCamera.transform.eulerAngles = new Vector3(initialRotation.x, initialRotation.y, initialRotation.z);
-        }
     }
 
+
+    private Vector2 pointOnMouseHold, pointOnMouseDown;
     void getCameraRotation()
     {
         if (Input.GetMouseButtonDown(1))
         {
-            p1 = Input.mousePosition;
+            pointOnMouseDown = Input.mousePosition;
         }
         if (Input.GetMouseButton(1))
         {
-            p2 = Input.mousePosition;
+            pointOnMouseHold = Input.mousePosition;
 
-            float dirX = (p1 - p2).x * rotationSpeed;
-            float dirY = (p1 - p2).y * rotationSpeed;
+            float dirX = (pointOnMouseDown - pointOnMouseHold).x * rotationSpeed * 0.001f;
 
-            transform.rotation *= Quaternion.Euler(new Vector3(0, -dirX, 0));
-            mainCamera.transform.rotation *= Quaternion.Euler(new Vector3(dirY, 0, 0));
+            cameraTransform.Rotate(0,-dirX,0,Space.World);        
         }
     }
 }
