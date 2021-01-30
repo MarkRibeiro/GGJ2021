83c9f3cca203d45670e4285f34f66c09fb2a9b2a
Mark Ribeiro <markpribeiro@hotmail.com>
2021-01-30 20:59:15 -0300

    som com slider

--- Assets/Scripts/VolumeSlider.cs
+++ Assets/Scripts/VolumeSlider.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class VolumeSlider : MonoBehaviour
 {
+    private Slider volumeSlider;
+    public AudioMixer gameMixer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        volumeSlider = GetComponent<Slider>();
+        gameMixer.SetFloat("mixerVolume", PlayerPrefs.GetFloat("Volume"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        volumeSlider.onValueChanged.AddListener(changeVolume);
+    }
+
+    void changeVolume(float currentValue)
+    {
+        PlayerPrefs.SetFloat("Volume", currentValue);
+        gameMixer.SetFloat("mixerVolume", currentValue);
     }
 }
