ae1ab24f82073a179a5ddf8a48b78a637504507b
NagibSuaid <nagibmoura@gmail.com>
2021-01-31 16:16:58 -0300

    sons na ilha grande

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public AudioClip arrg;
+    public AudioClip victoryClip;
+    public AudioClip picareta;
+    public AudioSource source;
+
+    private void OnEnable() {
+        StartCoroutine(Playsounds());
+    }
+
+    private IEnumerator Playsounds(){
+        source.PlayOneShot(victoryClip);
+        yield return new WaitForSeconds(5.0f);
+        source.PlayOneShot(arrg);
+
+    }
+}
---
+++ Assets/Scripts/TryAgain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TryAgain : MonoBehaviour
+{
+
+    public AudioClip failedClip;
+    public AudioSource source;
+    public AudioClip picareta;
+    private void OnEnable() {
+        source.PlayOneShot(picareta);
+        source.PlayOneShot(failedClip);
+    }
+ 
+}
