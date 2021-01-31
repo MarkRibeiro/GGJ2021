b4c75e2eefdbb9997cca18f6fcc552b0d39ffce6
NagibSuaid <nagibmoura@gmail.com>
2021-01-30 22:29:09 -0300

    Transição entre cenas

---
+++ Assets/Scripts/SceneTransition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneTransition : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public Animator fadeAnimation;
+    public float transitionDuration;
+    public IEnumerator ChangeScene(string scene)
+    {
+        float time = 0;
+        float ratioPerInterval = 1/transitionDuration;
+        float initialVolume = audioSource.volume;
+        fadeAnimation.SetTrigger("BeginCrossFade");
+        while(time<transitionDuration)
+        {
+            yield return new WaitForSeconds(0.05f);
+            time+=0.05f;
+            audioSource.volume = time * initialVolume / transitionDuration;
+        }
+        SceneManager.LoadScene(scene);
+    }
+}
