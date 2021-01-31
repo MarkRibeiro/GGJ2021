75a5c3469e20bb64585ef0d71f8b41d2a0ca6f7f
Mark Ribeiro <markpribeiro@hotmail.com>
2021-01-31 18:58:06 -0300

    Arruma varias paradas

---
+++ Assets/Scripts/TimeLapse.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeLapse : MonoBehaviour
+{
+    public CanvasGroup fadeInCanvas; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(stopTime());
+    }
+
+    private IEnumerator stopTime()
+    {
+        yield return new WaitUntil(() => fadeInCanvas.alpha <= 0);
+        Time.timeScale = 0;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = 1;
+    }
+}
