eb33eee0addaafa8ad1862b9a22855cee2f35d9e
NagibSuaid <nagibmoura@gmail.com>
2021-02-16 14:37:21 -0300

    impossivel enterrar no oceano

--- Assets/Scripts/ClickToHighlight.cs
+++ Assets/Scripts/ClickToHighlight.cs
@@ -25,32 +25,33 @@ public class ClickToHighlight : MonoBehaviour
             if( Input.GetMouseButtonDown(0)){
                 RaycastHit hit;
                 
-                if(Physics.Raycast(ray,out hit, 1000.0f)){
+                if(Physics.Raycast(ray,out hit, 1000.0f) && hit.transform.name != "Oceano"){
+                    Debug.Log(hit.transform.name);
                     matRenderer.sharedMaterial.SetVector("Vector3_C357230",hit.point);
-                }
 
-                if(TreasueManeger.treasure == false){
-                    treasure.confirmScreen.SetActive(true);
-                    Time.timeScale = 0;
-                    TreasueManeger.localOfTreasure = hit.point;
-                    TreasueManeger.treasure = true;
-                    Debug.Log("x= " + TreasueManeger.localOfTreasure.x + "y= " + TreasueManeger.localOfTreasure.y + "z= " + TreasueManeger.localOfTreasure.z);
-                }
-                if(TreasueManeger.treasure == true){
-                    if(Vector3.Distance(TreasueManeger.localOfTreasure, hit.transform.position) < 1.5)
-                    {
-                        gameOverScreen.SetActive(true);
-                        Debug.Log("Achou o tesouro.");
+                    if(TreasueManeger.treasure == false){
+                        treasure.confirmScreen.SetActive(true);
                         Time.timeScale = 0;
+                        TreasueManeger.localOfTreasure = hit.point;
+                        TreasueManeger.treasure = true;
+                        Debug.Log("x= " + TreasueManeger.localOfTreasure.x + "y= " + TreasueManeger.localOfTreasure.y + "z= " + TreasueManeger.localOfTreasure.z);
                     }
-                    else
-                    {
-                        wrongChoiceScreen.SetActive(true);
-                        Time.timeScale = 0;
-                        Debug.Log("x= " + hit.point.x + "y= " + hit.point.y + "z= " + hit.point.z);
+
+                    if(TreasueManeger.treasure == true){
+                        if(Vector3.Distance(TreasueManeger.localOfTreasure, hit.transform.position) < 1.5)
+                        {
+                            gameOverScreen.SetActive(true);
+                            Debug.Log("Achou o tesouro.");
+                            Time.timeScale = 0;
+                        }
+                        else
+                        {
+                            wrongChoiceScreen.SetActive(true);
+                            Time.timeScale = 0;
+                            Debug.Log("x= " + hit.point.x + "y= " + hit.point.y + "z= " + hit.point.z);
+                        }
                     }
                 }
-                
             }
         }
     }
